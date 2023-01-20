9612766c58f2431b9a9bdd52de7f27703a2849cc
Jamaicanheat <leckywm@mail.uc.edu>
2023-01-20 09:45:46 -0500

    Lab 2

---
+++ it3038c-scripts/bash/myFirstScript.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# 'This is a script. Hello!'
+greeting="This is a script. Hello!"
+echo $greeting, thanks for joining us!
+echo '$greeting, thanks for joining us! You owe me $20'
+echo "$greeting, thanks for joining us!"
+echo "$greeting, you owe me \$20"
