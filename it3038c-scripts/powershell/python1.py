418716f14e2ab982ce57a9ba3ccb0fd7dd65acb5
Jamaicanheat <leckywm@mail.uc.edu>
2023-02-22 12:18:06 -0500

    test commit

---
+++ it3038c-scripts/powershell/python1.py
@@ -0,0 +1,9 @@
+myName=input()
+
+while myName!='your name':
+    print('This is not "your name". Please type "your name":')
+    myName=input()
+
+print('Hello, ' + myName + '. That is a good name. How old are you?')
+
+myAge=int(input())
