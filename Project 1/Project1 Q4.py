6177845f35b87ae308dc235e27fc973bcc6bb477
Jamaicanheat <78461676+Jamaicanheat@users.noreply.github.com>
2023-02-24 12:51:09 -0500

    Delete Project1 Q4.py

--- Project 1/Project1 Q4.py
+++
@@ -1,9 +0,0 @@
-import psutil
-
-# Get the available disk space in bytes
-available_space = psutil.disk_usage('/').free
-
-# Convert the available space to gigabytes
-gb_available = round(available_space / (1024 * 1024 * 1024), 2)
-
-print(f"You have {gb_available} GB of free disk space.")
