348c7f5047b90555ce2530b4b60520b11c9fe1a9
Jamaicanheat <78461676+Jamaicanheat@users.noreply.github.com>
2023-02-24 13:02:27 -0500

    Add files via upload

---
+++ Project 1/project1 Q4.ps1
@@ -0,0 +1,16 @@
+﻿# Create a new instance of the SpeechSynthesizer class
+Add-Type -AssemblyName System.speech
+$speaker = New-Object System.Speech.Synthesis.SpeechSynthesizer
+
+# Set the voice to use (optional)
+$speaker.SelectVoice("Microsoft Zira Desktop")
+
+# Set the volume and rate of speech (optional)
+$speaker.Volume = 100
+$speaker.Rate = 0
+
+# Set the message to speak
+$message = "Hello, world! This is a test message."
+
+# Speak the message aloud
+$speaker.Speak($message)
